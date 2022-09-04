a63460ac26bc1d636a47026455935075c6982dce
ELENA <battilena@gmail.com>
2022-09-04 18:05:11 +0300

    task29

---
+++ HomeWork_task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+ Console.Clear();
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{ 
+    Console.Write($"Введите элемент массива под индексом {i}:\t "); // \t -отступ в виде табуляции
+    array[i] = int.Parse(Console.ReadLine());
+} 
+
+   Console.WriteLine("Вывод массива: ");
+
+   for (int i = 0; i < array.Length; i++)
+   {
+    Console.WriteLine(array[i]);
+   }
